1f46ae4dd987b5a021127f66d35b37f98980f1a1
Josh Bradley <jbradley@arcticlcc.org>
2015-10-05 17:14:32 -0800

    Resize navbar buttons based on navbar width.

--- json-schema-viewer.js
+++ json-schema-viewer.js
@@ -129,7 +129,7 @@ if (typeof JSV === 'undefined') {
             //responsive buttons
             var resizeBtn = function() {
                 var activePage = $.mobile.pageContainer.pagecontainer('getActivePage');
-                if ($(window).width() <= 800) {
+                if ($('.md-navbar').width() <= 800) {
                     $('.md-navbar .md-flex-btn.ui-btn-icon-left', activePage).toggleClass('ui-btn-icon-notext ui-btn-icon-left');
                 } else {
                     $('.md-navbar .md-flex-btn.ui-btn-icon-notext', activePage).toggleClass('ui-btn-icon-left ui-btn-icon-notext');
