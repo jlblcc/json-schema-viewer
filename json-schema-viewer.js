b8647e38b78968039ab0d8aaa528f3f9954ac6bc
Josh Bradley <jbradley@arcticlcc.org>
2016-09-27 16:04:53 -0800

    Fix global namespace jshint error

--- json-schema-viewer.js
+++ json-schema-viewer.js
@@ -3,13 +3,12 @@ if (!window.location.origin) {
   window.location.origin = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
 }
 
-
 if (typeof JSV === 'undefined') {
     /**
      * JSV namespace for JSON Schema Viewer.
      * @namespace
      */
-    var JSV = {
+    JSV = {
         /**
          * The root schema to load.
          */
